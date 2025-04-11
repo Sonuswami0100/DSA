a8283ec350a441d8342cc03a6777f5dfc592519a
sonuswami <sonusjag960@gmail.com>
2025-04-11 20:04:54 +0530

    rename file

--- codeJ/Hello.java
+++ codeJ/Hello.java
@@ -1,8 +1,8 @@
     class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        for(int i = 0;i <= 5;i++){
-            system.out.println(i);
-        }
+        // for(int i = 0;i <= 5;i++){
+        //     System.out.println(i);
+        // }
     }
 }
