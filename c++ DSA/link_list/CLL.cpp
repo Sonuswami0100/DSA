75c8fb5d4eea714e698ea63f57d4e0497e6155e0
sonuswami <sonusjag960@gmail.com>
2025-05-15 21:27:40 +0530

    add cll

--- c++ DSA/link_list/CLL.cpp
+++ c++ DSA/link_list/CLL.cpp
@@ -28,9 +28,74 @@ public:
             tail->next = head;
         }
     }
-    
+    void printval (){
+        if(head == NULL){
+            return;
+        }
+        cout<<head->data<<" -> ";
+        Node* temp = head->next;
+
+        while(temp != head){
+            cout<<temp->data<<" -> ";
+            temp = temp->next;
+        }
+        cout<<temp->data<<endl;
+    }
+    void insertatTail (int val){
+        Node* newNode = new Node(val);
+
+        if(tail == NULL){
+            head = tail = newNode;
+            tail->next = head;
+        }else{
+            newNode->next = head;
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    void deleteAtHead(){
+        if(head == NULL) return;
+        else if(head == tail){
+            delete head;
+            head = tail = NULL;
+        }else{
+            Node* temp = head;
+            head = head->next;
+            tail->next = head;
+
+            temp->next = NULL;
+            delete temp;
+        }
+    }
+    void deleteAttail(){
+        if(head == NULL){
+            return;
+        }else if(head == tail){
+            delete head;
+            head = tail = NULL;
+        }else{
+            Node* temp = tail;
+            Node* prev = head;
+            while(prev->next != tail){
+                prev = prev->next;
+            }
+            tail = prev;
+            tail->next = head;
+
+            temp->next = NULL;
+            delete temp;
+        }
+    }
 };
 int main(){
+    CLL l;
+    l.insertATHead(0);
+    l.insertATHead(1);
+    l.insertATHead(2);
+    l.insertATHead(3);
+    l.insertATHead(4);
+    l.insertATHead(5);
 
+    l.printval();
 return 0;
 }
